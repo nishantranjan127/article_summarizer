36f417192d429fd0fb81fafc6e9959ed34995d95
nishantranjan127 <nishantranjan127@gmail.com>
2024-01-25 00:05:24 +0530

    added project files

---
+++ app.py
@@ -0,0 +1,85 @@
+import streamlit as st
+from helper.helper import get_summary, spacy_rander, fetch_news, fetch_news_links
+
+
+st.set_page_config(
+     page_title="Data Analysis Web App",
+     page_icon="🧊",
+     layout="wide",
+     initial_sidebar_state="expanded",
+     menu_items={
+         'Get Help': 'https://github.com/everydaycodings/Text-Summarization-using-NLP',
+         'Report a bug': "https://github.com/everydaycodings/Text-Summarization-using-NLP/issues/new",
+         'About': "# This is a header. This is an *extremely* cool app!"
+     }
+)
+
+
+st.sidebar.title("Text Summarization Web App")
+
+option = ["News Summary and Headlines", "Custom Text Summarization"]
+choice = st.sidebar.selectbox("Select of your choice", options=option)
+
+
+if choice == "Custom Text Summarization":
+    st.sidebar.markdown("Copy Sample Article if you want to test the web app. [[article source](https://edition.cnn.com/2022/02/14/us/new-mexico-albuquerque-stabbings/index.html)]")
+    st.sidebar.code(open("presentation/sample.txt","r").read())
+    st.title("Welcome to {}".format(choice))
+
+    col1, col2 = st.columns(2)
+
+    with col1:
+        text = st.text_area(label="Enter Your Text or story", height=350, placeholder="Enter Your Text or story or your article iit can be of any length")
+        
+    if st.button("Get Summary and Headline"):
+        summary = get_summary(text)
+
+        try:
+            with col2:
+                st.write("Text Summary (Summary length: {})".format(len(summary)))
+                st.code(summary)
+                st.write("Text Headline")
+                st.code("Feature Comming Soon")
+
+            spacy_rander(summary)
+
+            #with st.expander("Get Original Article Analysis"):
+            spacy_rander(text, text="Yes")
+            
+        except NameError:
+            pass
+
+if choice == "News Summary and Headlines":
+    st.title("BBC News Summary")
+
+    search_query = st.text_input("", placeholder="Enter the topic you want to search")
+    st.write(" ")
+
+    link, title, thumbnail = fetch_news_links(search_query)
+    fetch_news = fetch_news(link)
+    
+    if link != []:
+        col1, col2 = st.columns(2)
+
+        with col1:
+            for i in range(len(link)):
+                if (i % 2) == 0:
+                    st.image(thumbnail[i])
+                    st.write(title[i])
+                    with st.expander("Read The Summary"):
+                        st.write(get_summary(fetch_news[i]))
+                    st.markdown("[**Read Full Article**]({})".format(link[i]), unsafe_allow_html=True)
+                    st.write(" ")
+        
+        with col2:
+            for i in range(len(link)):
+                if (i % 2) != 0:
+                    st.image(thumbnail[i])
+                    st.write(title[i])
+                    with st.expander("Read The Summary"):
+                        st.write(get_summary(fetch_news[i]))
+                    st.markdown("[**Read Full Article**]({})".format(link[i]), unsafe_allow_html=True)
+                    st.write(" ")
+    
+    else:
+        st.info("No Result found for {} Please try some popular Keywords".format(search_query))
---
+++ helper/helper.py
@@ -0,0 +1,159 @@
+from random import random
+from spacy.lang.en.stop_words import STOP_WORDS
+import en_core_web_sm
+from string import punctuation
+from heapq import nlargest
+import spacy_streamlit
+import requests
+import json
+from bs4 import BeautifulSoup
+import configparser
+import streamlit as st
+import random
+
+nlp= en_core_web_sm.load()
+#nlp= spacy.load("en_core_web_sm")
+stopwords = list(STOP_WORDS)
+punctuation = punctuation + "\n"
+
+config = configparser.ConfigParser()
+config.read("config.ini")
+news_api_key = config["API"]["news_api"]
+
+
+
+def spacy_rander(summary, text=None):
+
+    summ = nlp(summary)
+    if text == "Yes":
+        rend = spacy_streamlit.visualize_ner(summ, labels=nlp.get_pipe("ner").labels, title="Full Article Visualization", show_table=False, key=random.randint(0, 100))
+    
+    else:
+        rend = spacy_streamlit.visualize_ner(summ, labels=nlp.get_pipe("ner").labels, title="Summary Visualization", show_table=False, key=random.randint(0, 100))
+    
+    return rend
+
+
+
+def word_frequency(doc):
+    word_frequencies = {}
+
+    for word in doc:
+        if word.text.lower() not in stopwords:
+            if word.text.lower() not in punctuation:
+                if word.text not in word_frequencies.keys():
+                    word_frequencies[word.text] = 1
+                else:
+                    word_frequencies[word.text] += 1
+    
+    return word_frequencies
+
+
+
+
+def sentence_score(sentence_tokens, word_frequencies):
+    sentence_score = {}
+
+    for sent in sentence_tokens:
+        for word in sent:
+            if word.text.lower() in word_frequencies.keys():
+                if sent not in sentence_score.keys():
+                    sentence_score[sent] = word_frequencies[word.text.lower()] 
+                else:
+                    sentence_score[sent] += word_frequencies[word.text.lower()]
+    
+    return sentence_score
+
+
+@st.cache_data()
+def fetch_news_links(query):
+    link_list = []
+    title_list = []
+    thumbnail_list = []
+
+    if query == "":
+        reqUrl = "https://newsapi.org/v2/everything?sources=bbc-news&q=india&language=en&apiKey={}".format(news_api_key)
+    else:
+        reqUrl = "https://newsapi.org/v2/everything?sources=bbc-news&q={}&language=en&apiKey={}".format(query, news_api_key)
+    
+    headersList = {
+    "Accept": "*/*",
+    "User-Agent": "Thunder Client (https://www.thunderclient.com)" 
+    }
+
+    payload = ""
+
+    response = requests.request("GET", reqUrl, data=payload,  headers=headersList).text
+    response = json.loads(response)
+
+    tw = 0
+    for i in range(len(response["articles"])):
+        if tw ==10:
+            pass
+        else:
+            if "/news/" in response["articles"][i]["url"] and "stories" not in response["articles"][i]["url"]:
+                link_list.append(response["articles"][i]["url"])
+                title_list.append(response["articles"][i]["title"])
+                thumbnail_list.append(response["articles"][i]["urlToImage"])
+            else:
+                pass
+            tw += 1
+
+    return link_list, title_list, thumbnail_list
+
+
+
+@st.cache_data()
+def fetch_news(link_list):
+
+    news = []
+    news_list = []
+
+    for i in range(len(link_list)):
+        news_reqUrl = link_list[i]
+        headersList = {
+        "Accept": "*/*",
+        "User-Agent": "Thunder Client (https://www.thunderclient.com)" 
+        }
+
+        payload = ""
+
+        news_response = requests.request("GET", news_reqUrl, data=payload,  headers=headersList)
+        soup = BeautifulSoup(news_response.content, features="html.parser")
+        soup.findAll("p", {"class":"ssrcss-1q0x1qg-Paragraph eq5iqo00"})
+        soup.findAll("div", {"data-component":"text-block"})
+        for para in soup.findAll("div", {"data-component":"text-block"}):
+                news.append(para.find("p").getText())
+        joinnews = " ".join(news)
+        news_list.append(joinnews)
+        news.clear()
+    
+    return news_list
+
+
+
+
+def get_summary(text):
+    
+    doc = nlp(text)
+
+    word_frequencies = word_frequency(doc)
+    for word in word_frequencies.keys():
+        word_frequencies[word] = word_frequencies[word] / max(word_frequencies.values())
+    
+    sentence_tokens = [sent for sent in doc.sents]
+    sentence_scores = sentence_score(sentence_tokens, word_frequencies)
+
+    
+    select_length = int(len(sentence_tokens)*0.10)
+    summary  = nlargest(select_length, sentence_scores, key=sentence_scores.get)
+    summary = [word.text  for word in summary]
+    summary = " ".join(summary)
+
+    return summary
+
+
+
+
+
+    
